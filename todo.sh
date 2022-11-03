4f0ff32e890520db28bd093ec2a3b89667b72fe1
Ramiro Garcia <79334304+ranemirusG@users.noreply.github.com>
2022-11-03 16:48:44 -0300

    ahora si

---
+++ todo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# set TODO directory
+dir="$HOME/todo/"
+len=$(ls $dir | wc -l)
+
+# Show the options
+ls -1 $dir | sort | awk '{ print ++count")", $0}'
+echo "Select file by number: "
+
+# Input number, test, and open file with vim
+read inputNum
+if
+    [ $inputNum -le $len ] && [ $inputNum -gt 0 ] ; then
+    selectedPath=$(ls -1 $dir | awk -v x=$inputNum -v z=$dir 'NR==x {print z$0}')
+    fileName=(`basename $selectedPath`)
+    echo "Opening $fileName"
+    vim $selectedPath
+else
+    echo "Invalid option. Try again.\n"
+    todo
+fi
